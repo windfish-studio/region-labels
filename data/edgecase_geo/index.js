d4fce0dddb76004fa866a73ce4edfe58562203a3
Alex Pilafian <sikanrong@Alexs-MacBook-Pro.local>
2017-12-23 17:38:39 +0100

    Cleaning up a lot of logic in finding longestLine offset midpoints and extreme split start/end points. Avoids the risk of infinite looping while maintaining desired functionality

--- data/edgecase_geo/index.js
+++ data/edgecase_geo/index.js
@@ -4,5 +4,6 @@ module.exports = [
   require('./childress_tx.json'),
   require('./reagan_tx.json'),
   require('./roanoke_va.json'),
-  require('./wise_va.json')
+  require('./wise_va.json'),
+  require('./anua.json')
 ]
